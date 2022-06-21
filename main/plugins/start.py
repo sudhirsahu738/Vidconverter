e3df2af8bc077671ec064225daa7859e9d95398e
The_SHYAM <67130647+sudhirsahu738@users.noreply.github.com>
2022-06-21 13:33:22 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -26,9 +26,9 @@ from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_te
 async def start(event):
     await event.reply(f'{st}', 
                       buttons=[
-                              [Button.inline("Menu.", data="menu")]
-                          [Button.inline("Menu.", data="menu")]
-                          [Button.inline("Menu.", data="menu")]
+                          [Button.inline("Menu.", data="menu")
+                           Button.inline("Menu.", data="menu")
+                          Button.inline("Menu.", data="menu")]
                               ])
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
     await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
