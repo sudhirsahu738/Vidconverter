d74921fbf4ee705bd80c58fb9ce095a985155091
The_SHYAM <67130647+sudhirsahu738@users.noreply.github.com>
2022-06-21 13:49:12 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -27,7 +27,8 @@ async def start(event):
     await event.reply(f'{st}', 
                       buttons=[[Button.inline("Menu.", data="menu"),
                                Button.inline("Menu.", data="menu"), 
-                               Button.inline("Menu.", data="menu")]])
+                               Button.inline("Menu.", data="menu")], 
+                              [Button.url("Developer", url="https://t.me/MrKumarShyam")]])
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
     await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
     
